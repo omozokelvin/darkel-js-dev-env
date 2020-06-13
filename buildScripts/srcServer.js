2a27b9e4ce4bb25e5142a7ed07c6e5eb49c57ec8
Darkel Zezrucion <kelvinjune2@gmail.com>
2020-06-13 17:37:14 +0100

    added linting to our code

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,18 +1,34 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
 
-app.get('/', function (req, res) {
+//integrating webpack with express
+app.use(
+  require('webpack-dev-middleware')(compiler, {
+    // noInfo: true,
+    publicPath: config.output.publicPath,
+  })
+);
+
+//setting up express server
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    open('http://localhost:' + port);
-  }
+app.listen(port, (err) => {
+  // if (err) {
+  //   console.log(err);
+  // } else {
+  //   open('http://localhost:' + port);
+  // }
+
+  err ? console.log(err) : open(`http://localhost:${port}`);
 });
